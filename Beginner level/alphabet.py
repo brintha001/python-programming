e24d78b3bc25b4e2982fc677518f24ce28c67ce8
brintha001 <37359245+brintha001@users.noreply.github.com>
2018-03-20 14:39:51 +0530

    Create alphabet.py

---
+++ Beginner level/alphabet.py
@@ -0,0 +1,6 @@
+f= input()
+fat=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+if f  not in  fat:
+    print ("No")
+else:
+    print("alphabet")
