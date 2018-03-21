0b222c888b0b210232d28e43ac4c7e3c51403b97
brintha001 <37359245+brintha001@users.noreply.github.com>
2018-03-21 10:39:48 +0530

    Rename alphabet.py to alphabets.py

---
+++ Beginner level/alphabets.py
@@ -0,0 +1,6 @@
+e=input()
+group=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+if e in group:
+    print("Alphabet")
+else:
+    print("No")
