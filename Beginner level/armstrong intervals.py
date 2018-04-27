d0716a6d3ab197f0c29fcde948bf52b0d305512c
brintha001 <37359245+brintha001@users.noreply.github.com>
2018-04-27 10:35:47 +0530

    Update armstrong intervals.py

--- Beginner level/armstrong intervals.py
+++ Beginner level/armstrong intervals.py
@@ -3,8 +3,8 @@ for i in range(int(g)+1,int(h)):
     temp=i
     c=0
     while(i>0):
-    r=i%10
-    c=c+r*r*r
-    i=i//10
+        r=i%10
+        c=c+r*r*r
+        i=i//10
  if(c==temp):
-    print(temp,end='')
+     print(temp,end='')
