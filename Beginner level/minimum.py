24460561ce356703558f1e08b1f1d756212f10f5
brintha001 <37359245+brintha001@users.noreply.github.com>
2018-04-28 11:25:38 +0530

    Create minimum.py

---
+++ Beginner level/minimum.py
@@ -0,0 +1,9 @@
+nums =[2,3,1,45,5]
+nums.sort()
+length = len(nums)
+if (length % 2 == 0):
+    median = (nums[(length)//2] + nums[(length)//2-1]) / 2
+else:
+    median = nums[(length-1)//2]
+
+print(median)
