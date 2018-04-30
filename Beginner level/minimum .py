0b0edcf2d7b4a668d28577616bff3737315f05ff
brintha001 <37359245+brintha001@users.noreply.github.com>
2018-04-30 10:30:04 +0530

    Update minimum .py

--- Beginner level/minimum .py
+++ Beginner level/minimum .py
@@ -1,7 +1,7 @@
 def smallest_num_in_list( list ):
     min = list[ 0 ]
-    for a in list:
-        if a < min:
-            min = a
+    for b in list:
+        if b < min:
+            min = b
     return min
 print(smallest_num_in_list([2, 3, 1, 45,5]))
