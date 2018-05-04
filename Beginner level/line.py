e516a07e8a73bf61ecc7d06269d8b1fbcd0a8790
brintha001 <37359245+brintha001@users.noreply.github.com>
2018-05-04 14:29:53 +0530

    Create line.py

---
+++ Beginner level/line.py
@@ -0,0 +1,8 @@
+v1=int(input("enter the number: "))
+w1=int(input("enter the number: "))
+v2=int(input("enter the number: "))
+w2=int(input("enter the number: "))
+if ((v1*(w1-w2)+v2*(w2-w1))==0):
+    print ("Yes")
+else:
+    print ("No")
